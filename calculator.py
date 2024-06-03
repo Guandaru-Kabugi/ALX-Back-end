39b105f9fa596356b062b1c19aeecadb0025f75e
Guandaru-Kabugi <kabugi96@gmail.com>
2024-06-03 12:27:43 +0300

    new add

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 print("Welcome to this tip calculator.")
-bill = int(input("What was the total bill? "))
+billing = int(input("What was the total bill? "))
 tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
 no_of_pple_t0_split_with = int(input("How many people to split the bill? "))
-Amount_to_pay = ((tip/100) * bill + bill)/no_of_pple_t0_split_with
+Amount_to_pay = ((tip/100) * billing + billing)/no_of_pple_t0_split_with
 print("Each person should pay: {: .2f}" .format(Amount_to_pay))
